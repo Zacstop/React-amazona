8bc58f78f55443c006ac45c2c5bbeef19afa0e1b
zaca3392 <zaca3392@gmail.com>
2022-03-31 11:36:08 +0900

    lesson 68

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
+/* eslint-disable no-undef */
 import express from 'express';
 import data from './data.js';
 
 const app = express();
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not Found' });
+  }
+});
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
