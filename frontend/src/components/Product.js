823cb8bdb3fc344100e21a10e50732e991fb0d57
zaca3392 <zaca3392@gmail.com>
2022-04-02 09:19:42 +0900

    lesson 70

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 export default function Product(props) {
@@ -6,14 +7,14 @@ export default function Product(props) {
 
   return (
     <div key={product._id} className="card">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         {/* <!-- 680 x 830 --> */}
         <img className="medium" src={product.image} alt="product" />
-      </a>
+      </Link>
       <div className="card-body">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
-        </a>
+        </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <div className="price">$ {product.price}</div>
       </div>
