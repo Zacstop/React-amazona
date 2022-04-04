e88ba80128e883988375e18d4153b0dfdceaf045
zaca3392 <zaca3392@gmail.com>
2022-04-04 22:17:47 +0900

    lesson 76

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import data from './data.js';
-import userRouter from './routers/userrouter.js';
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
 dotenv.config();
 
@@ -16,25 +16,14 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useUnifiedTopology: true,
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
