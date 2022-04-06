52e4758fc3fb4cec7a6bbdb5ab7475566bd89547
zaca3392 <zaca3392@gmail.com>
2022-04-06 12:37:29 +0900

    lesson 78

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,9 +5,17 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducers';
+import {
+  userRegisterReducer,
+  userSigninReducer,
+} from './reducers/userReducers';
 
 const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -20,6 +28,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
